8c347d342b7481a2ddf55b52204df0014cb997e1
GABRIEL G WOLF <gabrielww20@gmail.com>
2023-08-08 20:31:25 -0300

    clase 08-08-23

---
+++ clase080823/ejercicio1/MiArchivoJs.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "EL PARRAFO HA CAMBIADO!!.";
+  }
+
+  function limpiar() {
+    document.getElementById("demo").innerHTML = "Un parrafo, que pasará si aprietas el boton?";
+  }
