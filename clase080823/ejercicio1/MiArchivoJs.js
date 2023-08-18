34a928d59a3bd1711823c8f717021b9d1f245e71
Gabriel G. Wolf <134015714+GabrielGWolf@users.noreply.github.com>
2023-08-17 21:31:42 -0300

    Delete clase080823 directory

--- clase080823/ejercicio1/MiArchivoJs.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "EL PARRAFO HA CAMBIADO!!.";
-  }
-
-  function limpiar() {
-    document.getElementById("demo").innerHTML = "Un parrafo, que pasará si aprietas el boton?";
-  }
